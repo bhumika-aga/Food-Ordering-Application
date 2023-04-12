a340696dc6c52985f72964df6077b80f80de60be
Bhumika Agarwal <bhumika.aga@gmail.com>
2023-04-12 19:00:04 +0530

    Updated Search Bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,49 @@
+import { useState } from "react";
 import { restaurantList } from "../config";
 import RestaurantCard from "./RestaurantCard";
 
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant.data.name.includes(searchText)
+  );
+  return filterData;
+}
+
 const Body = () => {
+  // let searchTxt = "KFC";
+  const [restaurants, setRestaurants] = useState(restaurantList);
+  const [searchText, setSearchText] = useState("KFC");
+
   return (
-    <div className="restaurant-list">
-      {restaurantList.map((restaurant) => {
-        //iterative method
-        return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
-      })}
-      {/* Naive Method */}
-      {/* <RestaurantCard {...restaurantList[0].data} />
-      <RestaurantCard {...restaurantList[1].data} />
-      <RestaurantCard {...restaurantList[2].data} />
-      <RestaurantCard {...restaurantList[3].data} />
-      <RestaurantCard {...restaurantList[4].data} />
-      <RestaurantCard {...restaurantList[5].data} /> */}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search Here..."
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        ></input>
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchText, restaurants);
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {restaurantList.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.data.id} {...restaurant.data} />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
